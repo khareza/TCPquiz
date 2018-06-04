ed1c08486bf3c22e2a453d236789d74d138fb04b
KamilHareza <kamil.hareza7@gmail.com>
2018-06-05 01:18:25 +0200

    Basic functionality: send request to server, receive response from server, connecting clients, loading questions, checking correct answer

--- TCPquiz Client/MainWindow.xaml.cs
+++ TCPquiz Client/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +23,149 @@ namespace TCPquiz_Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int counter = 0;
+        private int QuestionNumber = 0;
+
+        private static readonly Socket ClientSocket = new Socket
+      (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private const int PORT = 100;
         public MainWindow()
         {
             InitializeComponent();
+            ConnectToServer();
+            LoadQuestion();
+            
+           // RequestLoop();
+            //Exit();
+        }
+
+        private void LoadQuestion()
+        {
+            Question.Text = "";
+            for (int i = 0; i < 5; i++)
+            {               
+                Question.Text += File.ReadLines(@"c:\quest.txt").Skip(counter).Take(1).First() + "\r\n";
+                counter++;
+            }
+            counter++;
+            QuestionNumber++;
+        }
+
+        private void ConnectToServer()
+        {
+            int attempts = 0;
+
+            while (!ClientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    Question.Text+= "Connection attempt " + attempts + "\r\n";
+                    //Console.WriteLine("Connection attempt " + attempts);
+                    // Change IPAddress.Loopback to a remote IP to connect to a remote host.
+                    ClientSocket.Connect(IPAddress.Loopback, PORT);
+                }
+                catch (SocketException)
+                {
+                    Console.Clear();
+                }
+            }
+
+            //Console.Clear();
+            Question.Text = "";
+            Question.Text += "Connected" + "\r\n";
+            //Console.WriteLine("Connected");
+        }
+
+        private void RequestLoop()
+        {
+            Question.Text += @"<Type ""exit"" to properly disconnect client>" + "\r\n";
+            //Console.WriteLine(@"<Type ""exit"" to properly disconnect client>");
+
+            while (true)
+            {
+                //SendRequest();
+                ReceiveResponse();
+            }
+        }
+
+        /// <summary>
+        /// Close socket and exit program.
+        /// </summary>
+        private void Exit()
+        {
+            SendString("exit"); // Tell the server we are exiting
+            ClientSocket.Shutdown(SocketShutdown.Both);
+            ClientSocket.Close();
+            Environment.Exit(0);
+        }
+
+        private void SendRequest()
+        {
+            Question.Text += "Send a request: " + "\r\n";
+            //Console.Write("Send a request: ");
+            string request = "get date";
+            SendString(request);
+
+            if (request.ToLower() == "exit")
+            {
+                Exit();
+            }
+        }
+
+        /// <summary>
+        /// Sends a string to the server with ASCII encoding.
+        /// </summary>
+        private void SendString(string text)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(text);
+            ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+        private void ReceiveResponse()
+        {
+            var buffer = new byte[2048];
+            int received = ClientSocket.Receive(buffer, SocketFlags.None);
+            if (received == 0) return;
+            var data = new byte[received];
+            Array.Copy(buffer, data, received);
+            string text = Encoding.ASCII.GetString(data);
+            CheckerClient.Text = text;
+            //Console.WriteLine(text);
+        }
+
+        private void AnswA_Click(object sender, RoutedEventArgs e)
+        {
+            SendString(QuestionNumber.ToString());
+            SendString("A");
+            ReceiveResponse();
+            LoadQuestion();
+
+        }
+
+        private void AnswB_Click(object sender, RoutedEventArgs e)
+        {
+            SendString(QuestionNumber.ToString());
+            SendString("B");
+            ReceiveResponse();
+            LoadQuestion();
+        }
+
+        private void AnswC_Click(object sender, RoutedEventArgs e)
+        {
+            SendString(QuestionNumber.ToString());
+            SendString("C");
+            ReceiveResponse();
+            LoadQuestion();
+        }
+
+        private void AnswD_Click(object sender, RoutedEventArgs e)
+        {
+            SendString(QuestionNumber.ToString());
+            SendString("D");
+            ReceiveResponse();
+            LoadQuestion();
         }
     }
 }
--- TCPquiz/MainWindow.xaml.cs
+++ TCPquiz/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,14 +18,157 @@ using System.Windows.Shapes;
 
 namespace TCPquiz
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static readonly List<Socket> clientSockets = new List<Socket>();
+        private const int BUFFER_SIZE = 2048;
+        private const int PORT = 100;
+        private static readonly byte[] buffer = new byte[BUFFER_SIZE];
+        int QuestionNumber = 0;
+        private string CorrectAnswer = "";
+
+
         public MainWindow()
         {
             InitializeComponent();
+            
         }
+
+        private void LoadAnswer()
+        {
+            
+            CorrectAnswer = File.ReadLines(@"c:\quest.txt").Skip(QuestionNumber+4).Take(1).First();
+
+        }
+
+        private void StartServer_Click(object sender, RoutedEventArgs e)
+        {
+            SetupServer();
+        }
+
+        private void StopServer_Click(object sender, RoutedEventArgs e)
+        {
+            CloseAllSockets();
+        }
+
+        private void SetupServer()
+        {
+            Checker.Text += "Setting up server..." + "\r\n";
+           // Console.WriteLine("Setting up server...");
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, PORT));
+            serverSocket.Listen(0);
+            serverSocket.BeginAccept(AcceptCallback, null);
+            Status.Content = "Server is active";
+            //Console.WriteLine("Server setup complete");
+            Checker.Text += "Server setup complete" + "\r\n";
+        }
+        private void CloseAllSockets()
+        {
+            foreach (Socket socket in clientSockets)
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+
+            serverSocket.Close();
+            Status.Content = "Server is closed";
+        }
+
+        private void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket;
+
+            try
+            {
+                socket = serverSocket.EndAccept(AR);
+            }
+            catch (ObjectDisposedException) // I cannot seem to avoid this (on exit when properly closing sockets)
+            {
+                return;
+            }
+
+            clientSockets.Add(socket);
+            socket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, socket);
+            //Checker.Text += "Client connected, waiting for request..." + "\r\n";
+            //Console.WriteLine("Client connected, waiting for request...");
+            serverSocket.BeginAccept(AcceptCallback, null);
+        }
+
+        private void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket current = (Socket)AR.AsyncState;
+            int received;
+
+            try
+            {
+                received = current.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                //Checker.Text += "Client forcefully disconnected" + "\r\n";
+                //Console.WriteLine("Client forcefully disconnected");
+                // Don't shutdown because the socket may be disposed and its disconnected anyway.
+                current.Close();
+                clientSockets.Remove(current);
+                return;
+            }
+
+            byte[] recBuf = new byte[received];
+            Array.Copy(buffer, recBuf, received);
+            string text = Encoding.ASCII.GetString(recBuf);
+            //Checker.Text += "Received Text: " + text + "\r\n";
+            //Console.WriteLine("Received Text: " + text);
+            LoadAnswer();
+            if(int.TryParse(text, out QuestionNumber))
+            {
+                //xdxdxdxd
+            }
+            else
+            {
+                if (text.ToLower().Equals(CorrectAnswer.ToLower())) // Client requested time
+                {
+                    //Checker.Text += "Text is a get time request" + "\r\n";
+                    //Console.WriteLine("Text is a get time request");
+                    byte[] data = Encoding.ASCII.GetBytes("Good");
+                    current.Send(data);
+                    // Checker.Text += "Time sent to client" + "\r\n";
+                    //Console.WriteLine("Time sent to client");
+                }
+                else if (!text.ToLower().Equals(CorrectAnswer.ToLower()))
+                {
+                    byte[] data = Encoding.ASCII.GetBytes("Bad");
+                    current.Send(data);
+                }
+                else if (text.ToLower() == "exit") // Client wants to exit gracefully
+                {
+                    // Always Shutdown before closing
+                    current.Shutdown(SocketShutdown.Both);
+                    current.Close();
+                    clientSockets.Remove(current);
+                    //Checker.Text += "Client disconnected" + "\r\n";
+                    //Console.WriteLine("Client disconnected");
+                    return;
+                }
+                else
+                {
+                    //Checker.Text += "Text is an invalid request" + "\r\n";
+                    // Console.WriteLine("Text is an invalid request");
+                    byte[] data = Encoding.ASCII.GetBytes("Invalid request");
+                    current.Send(data);
+                    //Checker.Text += "Warning Sent" + "\r\n";
+                    //Console.WriteLine("Warning Sent");
+                }
+            }
+           
+
+            current.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
+        }
+
+
     }
 }
